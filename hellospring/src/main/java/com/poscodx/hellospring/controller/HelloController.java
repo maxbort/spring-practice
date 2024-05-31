6acff6e4cdd105bba0c24c0affc46c03875777a3
CHOI HyoJun <cjfgh2449@gmail.com>
2024-05-31 16:38:33 +0900

    feat. spring-practice emaillist #finish

--- hellospring/src/main/java/com/poscodx/hellospring/controller/HelloController.java
+++ hellospring/src/main/java/com/poscodx/hellospring/controller/HelloController.java
@@ -1,22 +1,62 @@
 package com.poscodx.hellospring.controller;
 
-import java.util.ArrayList;
+import java.io.IOException;
+import java.io.Writer;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HelloController {
-	
-	@ResponseBody
+
 	@RequestMapping("/hello")
-	public Object hello(String name) {
-		System.out.println(name);
-		return new ArrayList<String>(); //"/WEB-INF/views/hello.jsp";
+	public String hello() {
+		return "/WEB-INF/views/hello.jsp";
+	}
+
+	@RequestMapping("/hello2")
+	public String hello2(String name) {
+		System.out.println("name:" + name);
+		return "/WEB-INF/views/hello.jsp";
+	}
+
+	@RequestMapping("/hello3")
+	public ModelAndView hello3(String name) {
+		ModelAndView mav = new ModelAndView();
+		mav.addObject("name", name);
+		mav.setViewName("/WEB-INF/views/hello3.jsp");
+		
+		return mav;
 	}
 	
-	public void test() {
-		System.out.println("qwerqwerqwerwqer");
+	@RequestMapping("/hello4")
+	public String hello4(String name, Model model) {
+		model.addAttribute("name", name);
+		return "/WEB-INF/views/hello3.jsp";
+	}
+	
+	@ResponseBody
+	@RequestMapping("/hello5")
+	public String hello5() {
+		return "<h1>Hello World</h1>";
+	}
+
+	@RequestMapping("/hello6")
+	public String hello6() {
+		return "redirect:/hello";
+	}
+	
+	@RequestMapping("/hello7")
+	public void hello7(
+			HttpServletRequest request,
+			HttpServletResponse response,
+			Writer out) throws IOException {
+		response.getWriter().print("<h1>Hello World</h1>");
 	}
 }
